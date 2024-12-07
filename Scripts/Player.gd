a3e5767cc44713a17021490d2dcb9f51ee5a2deb
Evan Roberts <evanrobertsruby@gmail.com>
2024-12-07 18:10:56 -0500

    added world gen and z-level scrolling

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,29 +1,33 @@
 extends CharacterBody2D
 
-@export var move_speed = 15
-
-var world = TileMapLayer.new()
-var tile_position = Vector2i(0, 0)
+# Movement values
+@export var move_speed = 20
 var tile_size = 64.0
 var turn_counter = 0
 var y_dir = 0
 var x_dir = 0
 
+# World values
+@export var z_level = 47
+var tile_position = Vector2i(0, 0)
+var lower_layer = TileMapLayer.new()
+var curr_layer = TileMapLayer.new()
+
 func _ready() -> void:
 	pass
 
 func _process(delta: float) -> void:
+	print(z_level)
 	tile_position = Vector2i(self.position / tile_size)
 	
-	print(world.get_cell_source_id(tile_position))
-	
+	#print('standing on id: ' + str(world.get_cell_source_id(tile_position)))
 	handle_rotation()
 	
 	if turn_counter > move_speed:
 		turn_counter = 0
 		get_movement_input()
 		if can_move():
-			self.position = self.position + tile_size * Vector2(x_dir, -1 * y_dir)
+			self.position = self.position + (tile_size * Vector2(x_dir, -1 * y_dir))
 		y_dir = 0
 		x_dir = 0
 	elif turn_counter + (move_speed / 2) > move_speed:
@@ -31,14 +35,18 @@ func _process(delta: float) -> void:
 	
 	if Input.is_action_pressed("Interact"):
 		mine_block()
+		
+	if Input.is_action_just_pressed("Down"):
+		z_level -= 1
+	
+	if Input.is_action_just_pressed("Up"):
+		z_level += 1
 	
 	turn_counter+=1
 		
 func can_move() -> bool:
-	if world.get_cell_source_id(Vector2i((self.position.x / tile_size) + x_dir, (self.position.y / tile_size) - y_dir)) == -1:
-		return true
-	else:
-		return false
+	# is tile ahead air
+	return curr_layer.get_cell_source_id(Vector2i(tile_position.x + x_dir, tile_position.y - y_dir)) == -1 
 	
 func handle_rotation() -> void:
 	if y_dir != 0:
@@ -49,29 +57,30 @@ func handle_rotation() -> void:
 		
 func mine_block():
 	if self.rotation_degrees == 0: 		# Facing North
-		if world.get_cell_source_id(tile_position + Vector2i(0, -1)) == 0: # Is block minable?
-			world.set_cell(tile_position + Vector2i(0, -1), -1) # Mine block to North
+		if curr_layer.get_cell_source_id(tile_position + Vector2i(0, -1)) == 0: # Is block minable?
+			curr_layer.set_cell(tile_position + Vector2i(0, -1), -1) # Mine block to North
 	elif self.rotation_degrees == 90: 	# Facing East
-		if world.get_cell_source_id(tile_position + Vector2i(1, 0)) == 0: # Is block minable?
-			world.set_cell(tile_position + Vector2i(1, 0), -1) # Mine block to East
+		if curr_layer.get_cell_source_id(tile_position + Vector2i(1, 0)) == 0: # Is block minable?
+			curr_layer.set_cell(tile_position + Vector2i(1, 0), -1) # Mine block to East
 	elif self.rotation_degrees == -180: 	# Facing South
-		if world.get_cell_source_id(tile_position + Vector2i(0, 1)) == 0: # Is block minable?
-			world.set_cell(tile_position + Vector2i(0, 1), -1) # Mine block to South
+		if curr_layer.get_cell_source_id(tile_position + Vector2i(0, 1)) == 0: # Is block minable?
+			curr_layer.set_cell(tile_position + Vector2i(0, 1), -1) # Mine block to South
 	elif self.rotation_degrees == -90: 	# Facing West
-		if world.get_cell_source_id(tile_position + Vector2i(-1, 0)) == 0: # Is block minable?
-			world.set_cell(tile_position + Vector2i(-1, 0), -1) # Mine block to West
+		if curr_layer.get_cell_source_id(tile_position + Vector2i(-1, 0)) == 0: # Is block minable?
+			curr_layer.set_cell(tile_position + Vector2i(-1, 0), -1) # Mine block to West
 		
 		
-func set_world_info(new_world: TileMapLayer) -> void:
-	world = new_world
+func set_layer_info(new_lower_layer: TileMapLayer, new_curr_layer: TileMapLayer) -> void:
+	lower_layer = new_lower_layer
+	curr_layer = new_curr_layer
 	
 func get_movement_input() -> void:
-	var new_y_dir = Input.get_axis("Down", "Up")
+	var new_y_dir = Input.get_axis("South", "North")
 	if new_y_dir != 0:
 		x_dir = 0
 		y_dir = new_y_dir
 	
-	var new_x_dir = Input.get_axis("Left", "Right")
+	var new_x_dir = Input.get_axis("East", "West")
 	if new_x_dir != 0:
 		y_dir = 0
 		x_dir = new_x_dir
--- Scripts/TestScene.gd
+++ Scripts/TestScene.gd
@@ -1,21 +1,15 @@
 extends Node2D
 
+const DEFAULT_WORLD_LAYER = preload("res://Scenes/DefaultWorldLayer.tscn")
 
 var world = []
-var world_tile_set = preload("res://Assets/BlockTiles.tres")
-#var world_data = int[2][16][16]
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	
-	$Player.set_world_info($World/TileMap_Mid)
-
-	
-
 	#generate level
 	for z in range(0, 64):
-		var new_layer = TileMapLayer.new()
-		new_layer.tile_set = world_tile_set
-		world.append(new_layer)
+		var new_layer = DEFAULT_WORLD_LAYER.instantiate()
+		#new_layer.tile_set = world_tile_set
 		for x in range(0, 64):
 			for y in range(0, 64):
 				if z > 48:
@@ -27,16 +21,23 @@ func _ready() -> void:
 				else:
 					new_layer.set_cell(Vector2i(x, y), 0, Vector2i(0,0)) 	# Set cell to Bedrock
 				
-	
-	for i in range(0, 64):
-		for j in range(0, 64):
-			$World/TileMap_Btm.set_cell(Vector2i(i, j), 0, Vector2i(2, 0)) # Set floor tiles to dirt
-			if (i < 2 or i > 13) or (j < 2 or j > 13):
-				$World/TileMap_Mid.set_cell(Vector2i(i, j), 0, Vector2i(3, 0)) # Set tile to stone
-			else:
-				$World/TileMap_Mid.set_cell(Vector2i(i, j)) # Set tile to air
+				# Debug shaft to bottom of world
+				if x == 1 and y == 1:
+					new_layer.set_cell(Vector2i(x, y)) 						# Set cell to Air
+		new_layer.visible = false
+		#new_layer.z_index = z
+		world.append(new_layer)
+		$World.add_child(world[z])
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	for z in range(0, len(world)):
+		if z > $Player.z_level:
+			world[z].modulate = Color(1, 1, 1)
+			world[z].visible = false
+		else:
+			world[z].visible = true
+	world[$Player.z_level].modulate = Color(.8, .8, .8)
+	
 	pass
